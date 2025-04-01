488890be3cfc4d7fde0c392341db47566a37b4ce
yujinashrestha <yujinashresths@gmail.com>
2025-04-01 12:35:50 +0545

    hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, Setcounter]=useState(5)
+ const addvalue =() =>{
+console.log("value added", counter)
+if(counter<20){
+  counter=counter+1
+Setcounter(counter)
+}
+
+}
+ 
+
+ const removevalue=()=>{
+  console.log("value removed", counter)
+  
+  if(counter>0){
+    counter=counter-1
+Setcounter(counter)
+  }
+}
+
+  return (
+    <>
+      <h1>Chai aur Code</h1>
+      <h1>Counter value: {counter}</h1>
+      <button onClick={addvalue}>Add Value</button>
+      <br />
+      <button onClick={removevalue}>Remove Value</button>
+      
+    </>
+  )
+}
+
+export default App
