2996a18d86e65874baaa362c9e71c95c9f0e0725
yujinashrestha <yujinashresths@gmail.com>
2025-04-12 21:05:05 +0545

    mood app

---
+++ workshop/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './Home/Home'
+import { About } from './Home/About'
+import { UseState } from './usestate/UseState'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <UseState />
+    </>
+  )
+}
+
+export default App
---
+++ workshop/src/usestate/UseState.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+export const UseState = () => {
+
+    const [mood, setMood]=useState('🥰')
+    const happy=()=>{
+        setMood('😃')
+    }
+
+    const sad=()=>{
+        setMood('😢')
+    }
+
+    const laugh=()=>{
+        setMood('😂')
+    }
+  return (
+    <div>
+    <h1>Mood App</h1>
+    <h3>Mood: {mood}</h3>
+    <button onClick={happy}>Happy</button>
+    <button onClick={sad}>Sad</button>
+    <button onClick={laugh}>Laugh</button>
+    </div>
+  )
+}
